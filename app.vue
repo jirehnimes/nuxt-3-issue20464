bfdc1b4f711a2a9fc2ec58cd6fa82b9424140cd5
Jireh Nimes <jnimes@malvedaholdings.com>
2023-04-26 10:48:25 +0800

    Initialize

---
+++ app.vue
@@ -0,0 +1,31 @@
+<script setup>
+console.log('CLIENT LOG');
+
+const requestFromAPI = async () => {
+  console.log('EXECUTE API REQUEST');
+
+  const apiResult = await useFetch('/api/test');
+
+  console.log(apiResult?.data?.value);
+};
+</script>
+
+<template>
+  <div>
+    <button @click="requestFromAPI">
+      EXECUTE API REQUEST
+    </button>
+
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style>
+button {
+  padding: 24px 32px;
+  background-color: green;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
---
+++ server/api/test.js
@@ -0,0 +1,5 @@
+export default defineEventHandler((event) => {
+  console.log('SERVER API LOG');
+
+  return 'RESPONSE FROM SERVER API';
+});
